3fe3302fb6bb3a75190c378c7c2f852ad73592a6
smiller100 <srm08530@creighton.edu>
2020-02-23 21:02:40 -0600

    last update

---
+++ forms/calc/calc.js
@@ -0,0 +1,10 @@
+/*
+function calculate(n1,n2) {
+  let sum = n1 + n2
+  return sum
+  }
+  let num1 = parseInt(prompt("Number One"))
+  let num2 = parseInt(prompt("Number Two"))
+  let summedNumbers = calculate(num1, num2)
+  console.log(`The sum of ${num1} and ${num2} is ${summedNumbers}.`)
+  */
